13f28cddb5ff04d26644f34e1ef9e2a627d81c3d
Sadman-Miraj <sadmanhasanmiraj@gmail.com>
2026-01-02 23:50:48 +0600

    adding home service in book.php

--- Project/Php/book.php
+++ Project/Php/book.php
@@ -47,6 +47,37 @@
         </legend>
     </form>
 </div>
+
+<!----------------------------------template for home---------------------------------->
+<div id="homeService">
+
+    <form action="index.php" method="post">
+        <legend>Home Service Booking
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
+        <label for="address">Service Address:</label>
+        <input type="text" id="address" name="address" required><br><br>
+        <label for="date">Preferred Date:</label>
+        <input type="date" id="date" name="date" required><br><br>
+        
+        
+        <label for="type">Service Type:</label>
+        <select id="type" name="type">
+            <option value="oil">Oil Change</option>
+            <option value="tire">Tire Rotation</option>
+            <option value="brake">Brake Inspection</option>
+        </select>
+
+        <label for="coupon">Coupon Code:</label>
+        <input type="text" id="coupon" name="coupon"><br><br>
+        <button id="coupon Now">Apply</button>
+
+        <input type="submit" value="Book Now">
+        </legend>
+    </form>
+</div>
+<!----------------------------------template for emergency---------------------------------->
+
 <footer>
 </footer>
 </body>
