b2a3734eb4f1c08f226a18a8714958d6bb43858d
Sadman-Miraj <sadmanhasanmiraj@gmail.com>
2026-01-19 16:48:59 +0600

    adding session

---
+++ Project/Php/history.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+// Redirect to login if not logged in
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header("Location: auth/login.php");
+    exit();
+}
+
+include "../db/db.php";
+
+$user_name = $_SESSION['user_name'];
