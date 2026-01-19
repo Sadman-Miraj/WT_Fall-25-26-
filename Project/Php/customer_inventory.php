1ffbbcfb4faa09d4fc8a1b016b213dce314c4bca
Sadman-Miraj <sadmanhasanmiraj@gmail.com>
2026-01-19 17:34:45 +0600

    html str

--- Project/Php/customer_inventory.php
+++ Project/Php/customer_inventory.php
@@ -411,3 +411,14 @@ if ($result->num_rows > 0) {
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auto Parts Store - Automobiles Solution</title>
+    <link rel="stylesheet" href="../css/customer_inventory.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+    <div class="container">
