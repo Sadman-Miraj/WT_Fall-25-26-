ce9885115c9e72c64913ba0328ae5f57a3c66e9d
Sadman-Miraj <sadmanhasanmiraj@gmail.com>
2026-01-19 17:17:57 +0600

    html structure

--- Project/Php/forgot.php
+++ Project/Php/forgot.php
@@ -101,3 +101,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 $conn->close();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot Password</title>
+    <link rel="stylesheet" href="../css/login.css">
+    <style>
