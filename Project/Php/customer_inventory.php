e241eed8a56b3d28a471758dfe83fcddf449cef3
Sadman-Miraj <sadmanhasanmiraj@gmail.com>
2026-01-19 17:36:36 +0600

    catagory

--- Project/Php/customer_inventory.php
+++ Project/Php/customer_inventory.php
@@ -488,3 +488,11 @@ if ($result->num_rows > 0) {
         
         <!-- Message Area -->
         <div id="message" class="message"></div>
+                <!-- Category Filters -->
+        <div class="category-filters">
+            <button class="filter-btn active" data-category="all">All Products</button>
+            <button class="filter-btn" data-category="light">Lights</button>
+            <button class="filter-btn" data-category="wheel">Wheels</button>
+            <button class="filter-btn" data-category="shock">Shock Absorbers</button>
+            <button class="filter-btn" data-category="battery">Batteries</button>
+        </div>
